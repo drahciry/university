d38e6f68f025884991f091529a7901da5e5bc337
Daniel Quintao Feres <daniel.feres@tjrj.jus.br>
2025-11-18 10:14:22 -0300

    Fiz o exercicio 1247

--- Daniel/AED2/Beecrowd/1247.py
+++ Daniel/AED2/Beecrowd/1247.py
@@ -1,1 +1,13 @@
-# A fazer
+def solucao(D, VF, VG):
+    dguarda = ((12 * 12) + (D * D)) ** 0.5
+    dlarao = 12.0
+    tladrao = dlarao / VF
+    tguarda = dguarda / VG
+    return 'S' if tguarda <= tladrao else 'N'
+
+
+while True:
+    D = int(input())
+    VF, VG = map(int, input().split())
+    print(solucao(D, VF, VG))
+
