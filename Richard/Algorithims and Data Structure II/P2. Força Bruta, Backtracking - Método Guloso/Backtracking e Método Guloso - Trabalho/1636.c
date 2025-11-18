0738f5e2de01ad5532996b138c42b37834ba4155
drahciry <richardgonric@gmail.com>
2025-11-17 22:45:52 -0300

    1636 implementado

--- Richard/Algorithims and Data Structure II/P2. Força Bruta, Backtracking - Método Guloso/Backtracking e Método Guloso - Trabalho/1636.c
+++ Richard/Algorithims and Data Structure II/P2. Força Bruta, Backtracking - Método Guloso/Backtracking e Método Guloso - Trabalho/1636.c
@@ -1,45 +1,65 @@
 #include <stdio.h>
+#include <string.h>
 
-void permutacao(int n, int np, int P[], int S[]) {
+int eh_ciclica(int P[], int n) {
+    int j = 0;
+    int count = 0;
+
+    int visitados[n];
+    memset(visitados, 0, n * sizeof(int));
+
+    do {
+        if (visitados[j] == 1) return 0;
+        visitados[j] = 1;
+
+        j = P[j] - 1;
+        count++;
+    } while ((j != 0) && (count <= n));
+
+    return ((j == 0) && (count == n));
+}
+
+int permutacao(int n, int np, int P[], int S[]) {
     for (int i = 1; i <= n; i++) {
-        if ((S[i - 1] == 0)) {
-            P[np - 1] = i;
-            S[i - 1] = 1;
-            if (np == n) {
-                int j = 0;
-                int count = 0;
-                int modo = 0;
-                
-                if (P[1] < P[0])
-                    modo = 1;
-                else if (P[1] > P[0])
-                    modo = 2;
+        if (S[i - 1] == 1)
+            continue;
 
-                do {
-                    // if (modo == 1) {
-                    //     if (P[j] < P[j + 1])
-                    //         break;
-                    //     modo = 2;
-                    // } else {
-                    //     if (P[j] > P[j + 1])
-                    //        break;
-                    //     modo = 1;
-                    // }
-                    j = P[j] - 1;
-                    count++;
-                } while (j != 0);
+        P[np - 1] = i;
+        S[i - 1] = 1;
 
-                if (count == n) {
-                    for (int k = 0; k < n; k++)
-                        printf("%d ", P[k]);
-                    printf("\n");
-                }
-            } else {
-                permutacao(n, np + 1, P, S);
-            }
-            S[i - 1] = 0;
+        int poda = 0;
+        if (np > 2) {
+            int p_prev = P[np - 3];
+            int p_curr = P[np - 2];
+            int p_next = P[np - 1];
+            
+            if ((p_prev < p_curr) && (p_curr < p_next))
+                poda = 1;
+            if ((p_prev > p_curr) && (p_curr > p_next))
+                poda = 1;
         }
+        
+        if (poda == 1) {
+            S[i - 1] = 0;
+            continue;
+        }
+
+        if (np == n) {
+            if (eh_ciclica(P, n)) {
+                for (int k = 0; k < n; k++)
+                    printf("%d ", P[k]);
+                printf("\n");
+                return 1;
+            }
+        } else {
+            if (permutacao(n, np + 1, P, S))
+                return 1;
+        }
+
+        S[i - 1] = 0;
     }
+
+    return 0;
 }
 
 int main() {
@@ -52,9 +72,7 @@ int main() {
             break;
 
         int S[n], P[n];
-
-        for (int i = 0; i < n; i++)
-            S[i] = 0;
+        memset(S, 0, n * sizeof(int));
 
         permutacao(n, 1, P, S);
     }
