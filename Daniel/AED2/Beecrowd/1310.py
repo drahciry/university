ec8fd142fbb7dd91a2d600d5996d3e8f6a234538
Daniel Quintao Feres <daniel.feres@tjrj.jus.br>
2025-11-18 09:35:17 -0300

    fiz o exercicio 1310

--- Daniel/AED2/Beecrowd/1310.py
+++ Daniel/AED2/Beecrowd/1310.py
@@ -1,1 +1,20 @@
-# A fazer
+def solucao(dias: int, cuto_diario: int, receias: list):
+    if dias == 0:
+        return 0
+    
+    lucros_diarios = [receias[i] - cuto_diario for i in range(dias)]
+    lucro_total = 0
+    lucro_atual = 0
+
+    for i in lucros_diarios:
+        lucro_atual += i
+
+        if lucro_atual <= 0:
+            lucro_atual = 0
+
+        if lucro_total < lucro_atual:
+            lucro_total = lucro_atual
+    
+    return lucro_total
+
+print(solucao(6, 20, [18, 35, 6, 80, 15, 21]))
